ac78136ca575c807e5a0185692c539619fbfbd3a
Aidan Hui <139210594+aidanhui22@users.noreply.github.com>
2026-01-22 13:57:49 +1100

    Added Google Places API functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -7,6 +7,18 @@ function App() {
     "Thai", "Indian", "Korean", "Vietnamese", 
     "Greek", "American"
   ]);
+
+  const [location, setLocation] = useState('');
+  const handleChange = (e) => {
+    setLocation(e.target.value);
+  }
+
+  const [eateries, setEateries] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Location submitted: ${location}`);
+    getPlaces(location, cuisines);
+  }
 
   const removeCuisine = (cuisineToRemove) => {
     setCuisines(cuisines.filter(c => c !== cuisineToRemove));
@@ -20,6 +32,27 @@ function App() {
     ]);
   };
 
+  const getPlaces = async(location, cuisines) => {
+    const textQuery = `${cuisines[0]} and ${cuisines[1]} restaurants in ${location}`;
+    try {
+      const response = await fetch("https://places.googleapis.com/v1/places:searchText", {
+      method: "POST",
+      body: JSON.stringify({textQuery, pageSize: 10}),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        'X-Goog-FieldMask': 'places.displayName,places.formattedAddress',
+      }
+      });
+      if (!response.ok) {
+        throw new Error(`Status: ${response.status}`);
+      }
+      const data = await response.json();
+      setEateries(data.places);
+    } catch (error) {
+        console.error(error.message);
+    }
+  }
   return (
     <div>
       <h1 className='App-header'>uPick</h1>
@@ -35,6 +68,25 @@ function App() {
         <div>
           <h2 className='App-header'>Winner: {cuisines.join(' or ')}</h2>
         </div>
+      )}
+      <div className='Center'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter current location: <input type="text" value={location} onChange={handleChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      {eateries.length > 0 && (
+      <div>
+        <h3>Restaurants:</h3>
+        {eateries.map((place, index) => (
+          <div key={index}>
+            <p>{place.displayName?.text}</p>
+            <p>{place.formattedAddress}</p>
+          </div>
+        ))}
+      </div>
       )}
       <div className='Center'>
           <button className='Button' onClick={() => reset()}>Reset</button>
